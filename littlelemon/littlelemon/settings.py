c38d999bedb77939adf71dd7c61cb5095f6c2ba8
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-05-03 16:15:01 +0300

    feat: add REST Django and DB settings

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'USER': 'root',
+        'PASSWORD': 'Popcop1234567890',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
