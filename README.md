5ed01b99b9e8c947e57b6fb56ef580f7457b5554
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-05-04 13:04:41 +0300

    feat: new models and settings for MySQL connection

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # capstone_django
 Capstone project based on Django
+
+
+# Users 
+## SuperUser
+login: admin
+password: admin1234567890
+email: admin@inbox.lv
--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'reservations',
+        'NAME': 'LittleLemon',
         'USER': 'root',
         'PASSWORD': 'Popcop1234567890',
-        'HOST':'localhost',
+        'HOST':'127.0.0.1',
         'PORT':'3306',
+        'OPTIONS': {
+             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
